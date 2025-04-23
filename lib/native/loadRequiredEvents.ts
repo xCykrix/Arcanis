437aeb0ceb68feb4f33c41e953f29b4594bf2a18
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2025-04-23 14:11:08 +0000

    feat: add a upsert to clear global commands

--- lib/native/loadRequiredEvents.ts
+++ lib/native/loadRequiredEvents.ts
@@ -8,6 +8,7 @@ export function loadRequiredEvents(): void {
     optic.info(`[${packet.shardId}] Username: ${botUser.username} | Guilds: ${packet.guilds.length} | Application: ${packet.applicationId} | Session: ${packet.sessionId}.`);
 
     // Upsert Guild Commands
+    await Bootstrap.bot.helpers.upsertGlobalApplicationCommands([]);
     for (const guild of packet.guilds) {
       await Bootstrap.bot.helpers.upsertGuildApplicationCommands(guild, Bootstrap.interaction.values().toArray()).catch((e) => {
         createIncidentEvent(crypto.randomUUID(), 'Failed to upsertGlobalApplicationCommands.', e);
